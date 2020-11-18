22ad8c5ab56452da949162e4285aa1f3f36b3b69
Gabriel Barbosa <bielbarbosa900@gmail.com>
2020-11-18 10:17:17 -0300

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,49 @@
-import logo from './logo.svg';
+import React from 'react';
 import './App.css';
+import { api } from './api';
+import AllMovies from './Components/MovieList/AllMovies';
+import FeaturedMovie from './Components/FeaturedMovie';
+import Header from './Components/Header';
+import Footer from './Components/Footer';
+import Loading from './Components/Loading';
 
-function App() {
+const App = () => {
+  const [movieData, setMovieData] = React.useState([]);
+  const [featured, setFeatured] = React.useState({});
+
+  React.useEffect(() => {
+    async function pullMoviesData() {
+      const info = await api.movieInfo();
+      setMovieData(info);
+
+      const originalsMovies = info.filter((item) => item.slug === 'originals');
+      let randomNumber = Math.floor(
+        Math.random() * originalsMovies[0].item.results.length - 1,
+      );
+      if (randomNumber === 16) {
+        randomNumber += 1;
+      }
+      console.log(originalsMovies[0].item.results.length - 1);
+      const movieChosen = originalsMovies[0].item.results[randomNumber];
+      const chosenInfo = await api.pullMovieInfo(movieChosen.id, 'tv');
+      setFeatured(chosenInfo);
+    }
+    pullMoviesData();
+  }, []);
+
+  if (!movieData.length) return <Loading />;
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <Header />
+      {featured && <FeaturedMovie data={featured} />}
+      <section className="AllMovies">
+        {movieData.map((item) => (
+          <AllMovies key={item.title} title={item.title} items={item.item} />
+        ))}
+      </section>
+      <Footer />
     </div>
   );
-}
+};
 
 export default App;
